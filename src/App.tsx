892ad328b17da1a829a994f4c7c4132f5e31c532
Edwin Ramos <edwinramos@Edwins-MBP.nyc.rr.com>
2020-07-21 05:11:06 -0400

    added eslint, autodfixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 // import logo from './logo.svg';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 // import Form from 'react-bootstrap/Form'
 // import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -14,13 +14,13 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Recipe Searcher</Navbar.Brand>
-          <Nav className="mr-auto">
+        <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#about">About</Nav.Link>
           {/* <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-          </Nav>
-          {/* <Form inline>
+        </Nav>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-info">Search</Button>
           </Form> */}
@@ -29,7 +29,7 @@ function App() {
         <h2>
           Enter some ingredients below to create a list.
         </h2>
-        <p>After adding ingredients, press the submit button to get some recipes that contain your listed ingredients.</p>
+        <p>After adding ingredients, press submit to get recipes that contain your ingredients.</p>
         <p><Button variant="primary">Learn more</Button></p>
       </Jumbotron>
       <footer className="footer">
@@ -37,22 +37,22 @@ function App() {
       </footer>
     </div>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Edit <code>src/App.tsx</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  // </div>
   );
 }
 
