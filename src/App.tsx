21e2deb3ee3f845c19f99a60c20adb8ef707c919
Edwin Ramos <edwinramos@Edwins-MBP.nyc.rr.com>
2020-07-21 01:38:38 -0400

    some basic fill in stuff on landing, bootstrap components and css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+// import Form from 'react-bootstrap/Form'
+// import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Recipe Searcher</Navbar.Brand>
+          <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          {/* <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+          </Nav>
+          {/* <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+          </Form> */}
+      </Navbar>
+      <Jumbotron fluid>
+        <h2>
+          Enter some ingredients below to create a list.
+        </h2>
+        <p>After adding ingredients, press the submit button to get some recipes that contain your listed ingredients.</p>
+        <p><Button variant="primary">Learn more</Button></p>
+      </Jumbotron>
+      <footer className="footer">
+        <p>© Edwin J. Ramos, 2020</p>
+      </footer>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
